27abca87820fb0b9a316bf56fdbee539f6538e80
Rali Ralev <rali.ralev@fundingcircle.com>
2016-10-06 17:56:23 +0300

    Add Authorization and GuardianToken models

---
+++ web/models/guardian_token.ex
@@ -0,0 +1,28 @@
+defmodule RaliGuardian.GuardianToken do
+  use RaliGuardian.Web, :model
+
+  alias RaliGuardian.Repo
+  alias RaliGuardian.GuardianSerializer
+
+  @primary_key {:jti, :string, []}
+  @derive {Phoenix.Param, key: :jti}
+  schema "guardian_tokens" do
+    field :aud, :string
+    field :iss, :string
+    field :sub, :string
+    field :exp, :integer
+    field :jwt, :string
+    field :claims, :map
+
+    timestamps
+  end
+
+  def for_user(user) do
+    case GuardianSerializer.for_token(user) do
+      {:ok, aud} ->
+        (from t in RaliGuardian.GuardianToken, where: t.sub == ^aud)
+          |> Repo.all
+      _ -> []
+    end
+  end
+end
